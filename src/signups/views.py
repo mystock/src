7fe7e667c8fbffb39f12669f4af956de4506c3b0
csu <csu121@bu.edu>
2014-12-15 23:06:40 -0500

    add html and static files

---
+++ src/signups/views.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render, render_to_response, RequestContext, HttpResponseRedirect
+from django.contrib import messages
+# Create your views here.
+
+from .forms import SignUpForm
+
+def home(request):
+    
+    form = SignUpForm(request.POST or None)
+    
+    if form.is_valid():
+        save_it = form.save(commit=False)
+        save_it.save()
+        messages.success(request, 'We will be in touch!')
+        return HttpResponseRedirect('/thank-you/')
+    
+    return render_to_response("signup.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+
+
+def thankyou(request):
+    
+    return render_to_response("thankyou.html",
+                              locals(),
+                              context_instance=RequestContext(request))
+
+
+def aboutus(request):
+    
+    return render_to_response("aboutus.html",
+                              locals(),
+                              context_instance=RequestContext(request))
